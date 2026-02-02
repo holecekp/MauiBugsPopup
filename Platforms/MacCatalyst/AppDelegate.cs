30c3ddbfbbf85f23373742a347d335c95ecb1ab2
holecekp <24872113+holecekp@users.noreply.github.com>
2026-02-02 19:50:36 +0100

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBugsPopup
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
